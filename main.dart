a956a0c1d5b7ad9f7963902bd754fe9d8cf1121c
kashf99 <71818941+kashf99@users.noreply.github.com>
2022-03-10 07:23:38 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+main() {
+  String name = 'kashuf hameed';
+  print(name);
+  print(name.contains('h'));
+  print(name.endsWith('f'));
+  print(name.endsWith('d'));
+  print(name.isEmpty);
+  print(name.isNotEmpty);
+  print(name.length);
+  print(name.runes);
+}
