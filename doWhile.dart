5e748d9ff75afe4981b54007b25e5cead720cb49
kashf99 <71818941+kashf99@users.noreply.github.com>
2022-03-28 06:33:04 +0500

    Add files via upload

---
+++ doWhile.dart
@@ -0,0 +1,85 @@
+import 'dart:io';
+import 'dart:math';
+
+main() {
+  int num = 10;
+  do {
+    print(num);
+    num--;
+  } while (num > 0);
+  /*OUTPUT
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+ */
+  stdout.write("Enter two numbers:\nNumber 1:  ");
+  String? marksInStr1 = stdin.readLineSync();
+  stdout.write("Number 2:  ");
+  String? marksInStr2 = stdin.readLineSync();
+  int? num1 = int.tryParse(marksInStr1!) ?? 1;
+  int? num2 = int.tryParse(marksInStr2!) ?? 1;
+  int c = 1;
+  int r = 1;
+  do {
+    r = r * num1;
+    c++;
+  } while (c <= num2);
+  print("$num1^$num2 = $r");
+  /*OUTPUT
+Enter two numbers:
+Number 1:  12
+Number 2:  2
+12^2 = 144 */
+
+  stdout
+      .write("Enter a positive number to check if it is palindrome or  not:  ");
+  String? marksInStr3 = stdin.readLineSync();
+  int num3 = int.tryParse(marksInStr3!) ?? 1;
+  int n = num3;
+  int digit = 0;
+  int reverse = 0;
+  do {
+    digit = num3 % 10;
+    reverse = (reverse * 10) + digit;
+    num3 = num3 ~/ 10;
+  } while (num3 != 0);
+  print("Reverse of number = $reverse");
+  if (n == reverse) {
+    print("It is a palindrome");
+  } else {
+    print("it is not a palindrome");
+  }
+  /*OUTPUT
+   */
+  stdout.write("Enter two numbers:\nStarting number:  ");
+  String? marksInStr4 = stdin.readLineSync();
+  stdout.write("Ending number:  ");
+  String? marksInStr5 = stdin.readLineSync();
+  int start = int.tryParse(marksInStr4!) ?? 1;
+  int endd = int.tryParse(marksInStr5!) ?? 1;
+  int count = start;
+  do {
+    if (count % 2 != 0) {
+      print(count);
+    }
+    count++;
+  } while (count <= endd);
+
+  /*OUTPUT
+3
+5
+7
+9
+11
+13
+15
+17
+19 */
+}
